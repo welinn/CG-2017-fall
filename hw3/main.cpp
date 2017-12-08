b83434e3bb93a22374e2e68dd813463ad781f0c7
welinn <susutruelove@gmail.com>
2017-12-08 17:54:37 +0800

    finish

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -1,49 +1,32 @@
 #include "object.h"
 
-void animate();
 void render();
 void resizeFunction(int, int);
-//void rotRouteData();
-//void mouseFunc(int, int, int, int);
-//void mouseMotionFunc(int, int);
-void timer(int);
+void mouseFunc(int, int, int, int);
+void saveImg(bool);
 
 int currentWidth, currentHeight;
-int imgWidth = 400, imgHeight = 300;
+int imgWidth;
+int imgNum = 1;
 objects buck, santa, venus;
-//vector<glm::vec3> routeData;
-//int step = 0;
-//int totalLen;
-
 glm::vec3 cameraPos(0, 50, 100);
-//int roomAngle = 60;
-//double cameraRotAngle;
-//int mouseX, mouseY;
-//bool leftButtonDown = false;
-//bool rightButtonDown = false;
-/*
-void calAngle(){
-  glm::vec3 vector(0, 1, 0);
-//  if(cameraPos.z < 0) vector.z = -1;
-  cameraRotAngle = acos(dot(cameraPos, vector));
-  //if(cameraPos.z < 0) cameraRotAngle *= -1;
-}
-*/
+bool saving = false;
+
 int main(int argc, char* argv[]){
 
   char buckPath[] = "./data/Buck.obj";
   char santaPath[] = "./data/Santa Claus.obj";
   char venusPath[] = "./data/Venus_high_Stereo_withlogo.obj";
 
-  currentWidth = 1080;
-  currentHeight = 720;
+  currentWidth = imgWidth = 1024;
+  currentHeight = 768;
 
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
   glutInitWindowSize(currentWidth, currentHeight);
   glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
   glMatrixMode(GL_MODELVIEW);
-  int windowHandle = glutCreateWindow("midterm");
+  int windowHandle = glutCreateWindow("hw3");
   if(windowHandle < 1) {
     printf("ERROR: Could not create a new rendering window.\n");
     exit(EXIT_FAILURE);
@@ -82,17 +65,16 @@ int main(int argc, char* argv[]){
   calVerNormal(&santa, false);
   calVerNormal(&venus, false);
 
-  //float t = buck.size.z / 2;
-  rotateByVec(&buck, glm::vec3(0, 1, 0), 'z', glm::vec3(0, /*t*/0, 0), true);
-  rotateByVec(&santa, glm::vec3(0, 1, 0), 'z', glm::vec3(0, 0, 0), true);
-  rotateByVec(&venus, glm::vec3(0, 1, 0), 'z', glm::vec3(0, 0, -100), true);
+  rotateByVec(&buck, glm::vec3(0, 1, 0), 'z', glm::vec3(-60, 0, 0), true);
+  rotateByVec(&santa, glm::vec3(0, 1, 0), 'z', glm::vec3(60, 0, 0), true);
+  rotateByVec(&venus, glm::vec3(0, 1, 0), 'z', glm::vec3(0, 0, -400), true);
 
+  printf("Use mouse left button to enter output image size\n");
+  printf("Use mouse right button to save screenshot\n");
 
   glutReshapeFunc(resizeFunction);
   glutDisplayFunc(render);
-  //glutMouseFunc(mouseFunc);
-  //glutMotionFunc(mouseMotionFunc);
-  //glutTimerFunc(20, timer, 0);
+  glutMouseFunc(mouseFunc);
   glutMainLoop();
 
   return 0;
@@ -104,6 +86,61 @@ void resizeFunction(int width, int height){
   glViewport(0, 0, width, height);
 }
 
+void mouseFunc(int button, int state, int x, int y){
+  if(state == GLUT_DOWN){
+    if(button == GLUT_LEFT_BUTTON){
+      printf("Enter width: ");
+      scanf("%d", &imgWidth);
+      glutPostRedisplay();
+    }
+    else if(button == GLUT_RIGHT_BUTTON){
+      saving = true;
+      printf("Saving image...\n");
+      glutPostRedisplay();
+    }
+    printf("Use mouse left button to enter output image size\n");
+    printf("Use mouse right button to save screenshot\n");
+  }
+}
+
+void saveImg(bool save){
+  static std::vector<Mat> dstData;
+  if(save){
+    char path[20];
+    int imgHeight = imgWidth * currentHeight / currentWidth;
+    int len = dstData.size();
+    int rate = sqrt(len);
+    int x, y;
+    Mat dst(currentHeight * rate, currentWidth * rate, CV_8UC3);
+    Rect roi(0, 0, currentWidth, currentHeight);
+
+    sprintf(path, "./dst%d.jpg", imgNum++);
+    y = -currentWidth;
+    for(int i = 0; i < len ; i++){
+      if(i % rate == 0){
+        y += currentWidth;
+        x = currentHeight * (rate - 1);
+      }
+//      printf("x: %d, y: %d\n", x, y);
+      roi.x = y;
+      roi.y = x;
+      dstData[i].copyTo(dst(roi));
+      x -= currentHeight;
+
+    }
+    resize(dst, dst, Size(imgWidth, imgHeight), 0, 0, CV_INTER_AREA);
+    imwrite(path, dst);
+    printf("Image saved\n");
+    dstData.clear();
+  }
+  else{
+    Mat dst(currentHeight, currentWidth, CV_8UC3);
+    glReadPixels(0, 0, currentWidth, currentHeight, GL_BGR_EXT, GL_UNSIGNED_BYTE, dst.data);
+    flip(dst, dst, 0);
+    dstData.push_back(dst);
+  }
+}
+
 void render(){
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glViewport(0, 0, currentWidth, currentHeight);
@@ -111,53 +148,54 @@ void render(){
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
 
-  //gluPerspective(60, (double)currentWidth / currentHeight, 5, 4000);
-  glFrustum(-5, 5, -3, 3, 3, 2000);
-  gluLookAt(cameraPos.x, cameraPos.y, cameraPos.z, 0, 0, 0, 0, 1, 0);
-  glMatrixMode(GL_MODELVIEW);
+  float frustumWidth = (float) currentWidth / 200;
+  float frustumHeight = (float) currentHeight / 200;
 
-  animate();
+  if(!saving){
+    glFrustum(-frustumWidth, frustumWidth, -frustumHeight, frustumHeight, 3, 2000);
+    gluLookAt(cameraPos.x, cameraPos.y, cameraPos.z, 0, 0, 0, 0, 1, 0);
+    glMatrixMode(GL_MODELVIEW);
 
+    glPushMatrix();
+      drawObj(buck, true);
+      drawObj(santa, true);
+      drawObj(venus, true);
+    glPopMatrix();
+  }
+
+  else{
+    float tmpWidth;
+    float tmpHeight;
+    int rate = ceil((float)imgWidth / currentWidth);
+    float bufferSizeW = frustumWidth * 2 / rate;
+    float bufferSizeH = frustumHeight * 2 / rate;
+    tmpWidth = -frustumWidth;
+    for(int i = 0; i < rate; i++){
+      tmpHeight = -frustumHeight;
+      for(int j = 0; j < rate; j++){
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glViewport(0, 0, currentWidth, currentHeight);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glFrustum(tmpWidth, tmpWidth + bufferSizeW, tmpHeight, tmpHeight + bufferSizeH, 3, 2000);
+        gluLookAt(cameraPos.x, cameraPos.y, cameraPos.z, 0, 0, 0, 0, 1, 0);
+        glMatrixMode(GL_MODELVIEW);
+
+        glPushMatrix();
+          drawObj(buck, true);
+          drawObj(santa, true);
+          drawObj(venus, true);
+          saveImg(false);
+        glPopMatrix();
+        tmpHeight += bufferSizeH;
+      }
+      tmpWidth += bufferSizeW;
+    }
+    saveImg(true);
+
+    saving = false;
+  }
   glFlush();
   glutSwapBuffers();
-  //glutPostRedisplay();
 
 }
-
-void animate(){
-
-/*
-  glm::vec3 vec = routeData[(step + 1) % totalLen] - routeData[step];
-  glm::vec3 trans = routeData[(step + 1) % totalLen] - doll.position;
-  rotateByVec(&doll, vec, 'z', trans);
-  if(step % 5 == 0){
-    glm::vec3 g = glm::vec3(0, -1, 0);
-    glm::vec3 hori = cross(g, doll.axis.z);
-    glm::vec3 top = cross(hori, doll.axis.z);
-    float dot1, dot2;
-    dot1 = acos(dot(top, doll.axis.y, true));
-    dot2 = acos(dot(-top, doll.axis.y, true));
-    if(dot1 < 0) dot1 += 2 * M_PI;
-    if(dot2 < 0) dot2 += 2 * M_PI;
-    if(dot1 < dot2) doll.axis.y = top;
-    else doll.axis.y = -top;
-  }
-*/
-
-  glPushMatrix();
-    drawObj(buck, true);
-    drawObj(venus, true);
-  glPopMatrix();
-
-  glPushMatrix();
-    drawObj(santa, true);
-  glPopMatrix();
-/*
-  step = ++step % totalLen;
-*/
-}
-/*
-void timer(int t){
-  glutTimerFunc(50, timer, t + 1);
-  glutPostRedisplay();
-}*/
